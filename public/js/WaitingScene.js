fb428e2eeb38156917c6a50ebca82753d2c39507
Ilja Pavlovs <pavlovs.ilja@gmail.com>
2025-03-21 16:30:28 +0200

    back to lobby button

--- public/js/WaitingScene.js
+++ public/js/WaitingScene.js
@@ -7,7 +7,33 @@ class WaitingScene extends Phaser.Scene {
       this.roomId = data.roomId;
     }
 
+    preload() {
+      this.load.image('homeButton', 'assets/home-button.png');
+      this.load.audio('clickSound', 'assets/click.mp3');
+    }
+
     create() {
+      this.clickSound = this.sound.add('clickSound');
+      
+      // Add home button in the top-left corner
+      this.homeButton = this.add.image(40, 40, 'homeButton')
+        .setOrigin(0.5)
+        .setScale(0.6)
+        .setInteractive()
+        .on('pointerdown', () => {
+          this.clickSound.play();
+          this.returnToLobby();
+        });
+        
+      // Add hover effect
+      this.homeButton.on('pointerover', () => {
+        this.homeButton.setScale(0.7);
+      });
+      
+      this.homeButton.on('pointerout', () => {
+        this.homeButton.setScale(0.6);
+      });
+
       this.waitingText = this.add.text(
         this.cameras.main.width / 2, 
         this.cameras.main.height / 2 - 50, 
@@ -29,5 +55,29 @@ class WaitingScene extends Phaser.Scene {
         'Share this ID with your friend',
         { fontSize: '20px', fill: '#aaa' }
       ).setOrigin(0.5);
+      
+      // Setup browser back button handling
+      window.onpopstate = () => {
+        this.returnToLobby();
+      };
     }
-  }
+    
+    returnToLobby() {
+      // Tell server we're leaving the room
+      window.socket.emit('returnToLobby');
+      
+      // Remove room ID from URL
+      if (window.history && window.history.pushState) {
+        window.history.pushState("", document.title, window.location.pathname);
+      }
+      
+      // Return to lobby
+      this.scene.start('MenuScene');
+      this.scene.stop('WaitingScene');
+    }
+    
+    shutdown() {
+      // Clean up browser back button handler
+      window.onpopstate = null;
+    }
+}
